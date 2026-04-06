b8fae3d5762a48f130fef5213c6877f4efbc20f3
Hobr <mail@hobr.site>
2026-04-06 15:03:01 +0800

    flake: 迁移flake-utils到flakelight

--- flake.nix
+++ flake.nix
@@ -3,77 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flakelight.url = "github:nix-community/flakelight";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-      rust-overlay,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [
-          rust-overlay.overlays.default
-          (final: prev: {
-            rustToolchain =
-              let
-                rust = prev.rust-bin;
-              in
-              if builtins.pathExists ./rust-toolchain.toml then
-                rust.fromRustupToolchainFile ./rust-toolchain.toml
-              else if builtins.pathExists ./rust-toolchain then
-                rust.fromRustupToolchainFile ./rust-toolchain
-              else
-                rust.stable."1.94.1".default.override {
-                  extensions = [
-                    "rust-src"
-                    "rustfmt"
-                    "rust-analyzer"
-                    "clippy"
-                    "cargo"
-                  ];
-                  targets = [
-                    "wasm32-wasip2"
-                  ];
-                };
-          })
-        ];
-        pkgs = import nixpkgs { inherit overlays system; };
-      in
-      {
-        devShells.default =
-          with pkgs;
-          mkShell rec {
-            packages = [
-              pkg-config
-              openssl
+    { flakelight, ... }@inputs:
+    flakelight ./. {
+      inherit inputs;
 
-              rustToolchain
-              cargo-binstall
-              pre-commit
-              protobuf
-              just
-            ];
+      withOverlays = [
+        inputs.rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain =
+            let
+              rust = prev.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable."1.94.1".default.override {
+                extensions = [
+                  "rust-src"
+                  "rustfmt"
+                  "rust-analyzer"
+                  "clippy"
+                  "cargo"
+                ];
+                targets = [
+                  "wasm32-wasip2"
+                ];
+              };
+        })
+      ];
 
-            env = {
-              LD_LIBRARY_PATH = lib.makeLibraryPath packages;
-              RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+      devShell =
+        pkgs: with pkgs; rec {
+          packages = [
+            pkg-config
+            openssl
+            rustToolchain
+            cargo-binstall
+            pre-commit
+            protobuf
+            just
+          ];
 
-              PROTOC = "${protobuf}/bin/protoc";
-              PROTOC_INCLUDE = "${protobuf}/include";
-            };
-
-            shellHook = ''
-              export CARGO_HOME="$PWD/.cargo"
-              export PATH="$CARGO_HOME/bin:$PWD/target/debug:$PATH"
-            '';
+          env = {
+            LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            PROTOC = "${protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${protobuf}/include";
           };
-      }
-    );
+
+          shellHook = ''
+            export CARGO_HOME="$PWD/.cargo"
+            export PATH="$CARGO_HOME/bin:$PWD/target/debug:$PATH"
+          '';
+        };
+    };
 }
