3bc9c1a073a20df339b9e392bd4821c704062001
Hobr <mail@hobr.site>
2026-04-11 13:03:48 +0800

    remake

--- crate/shiroha-client/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod client;
-mod flow;
-mod job;
-mod job_support;
-mod manifest;
-
-pub use client::ControlClient;
-pub use flow::{FlowDetails, FlowVersionSummary, ForceDeleteFlowResult};
-pub use job::{EventQuery, ForceDeleteJobResult, JobDetails, JobEvent};
